4994ba6e0a56ae9fb7edc277784128c1b746c58e
neil <msg2neil@gmail.com>
2026-09-05 10:03:07 +0800

    ✨ feat: distinguish Vite development page title

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./App";
 import "./styles.css";
 
+if (import.meta.env.DEV) {
+  document.title = `[vite] ${document.title}`;
+}
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
